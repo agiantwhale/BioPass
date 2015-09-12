9c2f1070cd06a962dc666af6c5ea69a96a6ee5e3
Kevin <kzen@uchicago.edu>
2015-09-12 08:20:47 -0700

    added 2 twitter and one facebook login

--- siteinfo.js
+++ siteinfo.js
@@ -9,4 +9,21 @@ function siteinfo() {
       loginButton: $('.flex-table-secondary button.submit')
     };
   }
+  if(s.include(currentUrl,'twitter')) {
+    return {
+      loginScreen: $('input#signin-email').length===1,
+      loginInput: $('input#signin-email'),
+      passwordInput: $('input#signin-password'),
+      loginButton: $('.Button StreamsLogin js-login')
+    };
+  }
+
+  if(s.include(currentUrl,'facebook')) {
+    return {
+      loginScreen: $('input#email').length===1,
+      loginInput: $('input#email'),
+      passwordInput: $('input#pass'),
+      loginButton: $('input#loginbutton')
+    };
+  }
 }
