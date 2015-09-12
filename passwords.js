c0667feb1255aecf911ea150333af844c69f07ea
Il Jae Lee <iljae@umich.edu>
2015-09-12 01:33:54 -0400

    Added asyncjs

--- passwords.js
+++ passwords.js
@@ -1,9 +1,48 @@
-function AuthMethod(callback){
-  callback(true,token);
-}
+var audioAuthMethod={
+  userMedia:{
+    audio:true,
+    video:false
+  },
+  auth:function(payload,callback){
+    callback(true,token);
+  }
+};
+
+var videoAuthMethod={
+  userMedia:{
+    audio:false,
+    video:true
+  },
+  auth:function(payload,callback){
+    callback(true,token);
+  }
+};
+
 
 function AuthInfoStore(url,callback){
+  if(s.include(url,'www.facebook.com')){
+    callback("username","password");
+  }
 }
 
-function AuthManager(){
+function AuthManager(){ // Implementations of AuthMethod passed to arguments
+  this.authStrategies=_.toArray(arguments);
 }
+
+AuthManager.prototype.attemptAuth=function(callback){
+  var authMgr=this;
+
+  _.each(authMgr.authStrategies,function(strategy){
+
+    navigator.webkitGetUserMedia(
+      strategy.userMedia,
+      function(stream){
+      },
+      function(error){
+        alert('Unable to access device!');
+      }
+    );
+
+  });
+};
+
